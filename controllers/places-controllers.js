debf75abedc49bd13fb4569df0d4550784dc2edc
Keshav Pandey <88835211+RayLikesAnime@users.noreply.github.com>
2024-06-02 17:31:12 +0530

    users routes added

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -30,16 +30,16 @@ if(!place){
 res.json({place: place});
 };
 
-const getPlaceByUserId=(req,res,next)=>{
+const getPlacesByUserId=(req,res,next)=>{
     const userId=req.params.uid;
-    const place=DUMMY_PLACES.find(p=>{
+    const places=DUMMY_PLACES.filter(p=>{
         return p.creator===userId;
     });
-    if(!place){
-        return next (new HttpError('Couldnt find a place for the provided user id',404));
+    if(!places || places.length===0){
+        return next (new HttpError('Couldnt find  places for the provided user id',404));
     }
 
-    res.json({place: place});
+    res.json({place: places});
 }
 
 const createPlace=(req,res,next)=>{
@@ -78,7 +78,7 @@ const deletePlace=(req,res,next)=>{
 };
 
 exports.getPlaceById=getPlaceById;
-exports.getPlaceByUserId=getPlaceByUserId;
+exports.getPlacesByUserId=getPlacesByUserId;
 exports.createPlace=createPlace;
 exports.updatePlace=updatePlace;
 exports.deletePlace=deletePlace;
