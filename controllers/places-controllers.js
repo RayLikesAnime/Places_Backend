59c736586c55b086392d6291f5f1b1240e57093f
Keshav Pandey <88835211+RayLikesAnime@users.noreply.github.com>
2024-06-05 16:08:18 +0530

    backend connected to database complete

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -4,21 +4,7 @@ const {uuid}=require('uuidv4');
 const getCoordsForAddress=require('../util/location');
 const Place=require('../models/place');
 const mongoose=require('mongoose');
-
-
-let DUMMY_PLACES=[
-    {
-        id:'p1',
-        title:'Empire State Building',
-        description:'One of the most famous sky scrapers in the world!',
-        address:'20 W 34th St, New York, NY 10118, United States',
-        location:{
-            lat:40.7484405,
-            lng:-73.9878531
-        },
-        creator:'u1'
-    }
-];
+const User=require('../models/user');
 
 const getPlaceById= async(req,res,next) => {
     const placeId=req.params.pid;
@@ -41,19 +27,19 @@ res.json({place: place.toObject({getters:true})});
 
 const getPlacesByUserId=async(req,res,next)=>{
     const userId=req.params.uid;
-    let places;
+    let userWithPlaces;
 
     try{
-        places=await Place.find({creator:userId});
+        userWithPlaces=await User.findById(userId).populate('places');
     }catch(err){
         const error=new HttpError('Fetching places failed, please try again later',500);
         return next(error);
     }
-    if(!places || places.length===0){
+    if(!userWithPlaces || userWithPlaces.places.length===0){
         return next (new HttpError('Couldnt find  places for the provided user id',404));
     }
 
-    res.json({places: places.map(place=>place.toObject({getters:true}))});
+    res.json({places: userWithPlaces.places.map(place=>place.toObject({getters:true}))});
 }
 
 const createPlace=async(req,res,next)=>{
@@ -81,8 +67,28 @@ const createPlace=async(req,res,next)=>{
         creator
     });
 
+    let user;
     try{
-        await createdPlace.save();
+        user=await User.findById(creator);
+    }
+    catch(err){
+        const error=new HttpError('Creating place failed, please try again',500);
+        return next(error);
+    }
+
+    if(!user){
+        const error=new HttpError('Couldnt find user for provided id',404);
+        return next(error);
+    }
+    console.log(user);
+
+    try{
+        const sess=await mongoose.startSession();
+        sess.startTransaction();
+        await createdPlace.save({session:sess});
+        user.places.push(createdPlace);
+        await user.save({session:sess});
+        await sess.commitTransaction();
     }catch(err){
         const error=new HttpError('creating place failed',500);
         return next (error);
@@ -95,7 +101,7 @@ const updatePlace=async(req,res,next)=>{
 
     const errors=validationResult(req);
     if(!errors.isEmpty()){
-        throw new HttpError('Invalid inputs passed, please check your data',422);
+        return next( new HttpError('Invalid inputs passed, please check your data',422));
     }
 
     const{ title,description }=req.body;
@@ -127,14 +133,36 @@ const updatePlace=async(req,res,next)=>{
 
 };
 
-const deletePlace=(req,res,next)=>{
+const deletePlace=async (req,res,next) => {
     const placeId=req.params.pid;
 
-    if(!DUMMY_PLACES.find(p=> p.id===placeId)){
-        throw new HttpError('Couldnt find a place for that id.',404);
+    let place;
+    try{
+        place= await Place.findById(placeId).populate('creator');
+    }catch(err){
+        const error=new HttpError('Couldnt delete place',500);
+        return next(error);
+    }
+
+    if(!place){
+        const error=new HttpError('Couldnt find place for this id',404);
+        return next(error);
     };
 
-    DUMMY_PLACES=DUMMY_PLACES.filter(p=> p.id!==placeId);
+
+
+    try{
+        const sess=await mongoose.startSession();
+        sess.startTransaction();
+        await place.deleteOne({session:sess});
+        place.creator.places.pull(place);
+        await place.creator.save({session:sess});
+        await sess.commitTransaction();
+    }catch(err){
+        const error=new HttpError('Couldnt delete placedfdf',500);
+        return next(error);
+    }
+
     res.status(200).json({message:'Deleted Place!'});
 };
 
--- models/place.js
+++ models/place.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose');
 const Schema=mongoose.Schema;
 
 const placeSchema=new Schema({
-    creator:{type:String, required:true},
+    creator:{type:mongoose.Types.ObjectId, required:true, ref:'User'},
     title:{type:String, required:true},
     description:{type:String, required:true},
     image:{type:String, required:true},
