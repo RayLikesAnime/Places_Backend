23d15e153bd5a9ade12ec573641c4890a0cbfe74
Keshav Pandey <88835211+RayLikesAnime@users.noreply.github.com>
2024-06-16 03:43:46 +0530

    image upload completed

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -63,7 +63,7 @@ const createPlace=async(req,res,next)=>{
         description,
         address,
         location: coordinates,
-        image:'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg/330px-Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg',
+        image: req.file.path ,
         creator
     });
 
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const {check}=require('express-validator');
+const fileUpload=require('../middleware/file-upload')
 
 const PlacesControllers=require('../controllers/places-controllers')
 
@@ -10,7 +11,7 @@ router.get('/:pid',PlacesControllers.getPlaceById);
 
 router.get('/user/:uid',PlacesControllers.getPlacesByUserId);
 
-router.post('/',[check('title').not().isEmpty(), check('description').isLength({min:5}), check('address').not().isEmpty() ],PlacesControllers.createPlace);
+router.post('/', fileUpload.single('image') , [check('title').not().isEmpty(), check('description').isLength({min:5}), check('address').not().isEmpty() ],PlacesControllers.createPlace);
 
 router.patch('/:pid',[check('title').not().isEmpty(), check('description').isLength({min:5})],PlacesControllers.updatePlace);
 
