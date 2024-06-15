f62c9a15b19deee3249c350ede61eb284c67de33
Keshav Pandey <88835211+RayLikesAnime@users.noreply.github.com>
2024-06-16 03:23:13 +0530

    code tweaked for images upload

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -5,10 +5,12 @@ const UsersControllers=require('../controllers/users-controllers')
 
 const router=express.Router();
 
+const fileUpload=require('../middleware/file-upload')
+
 
 router.get('/',UsersControllers.getUsers);
 
-router.post('/signup', [check('name').not().isEmpty(),check('email').normalizeEmail().isEmail(),check('password').isLength({min:6})] ,UsersControllers.signup);
+router.post('/signup',fileUpload.single('image') ,[check('name').not().isEmpty(),check('email').normalizeEmail().isEmail(),check('password').isLength({min:6})] ,UsersControllers.signup);
 
 router.post('/login', UsersControllers.login);
 
